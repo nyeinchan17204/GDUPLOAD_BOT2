3ef9fbf2c3de814e857ad44b52dea6539d2395ec
nyeinchan17204 <92505220+nyeinchan17204@users.noreply.github.com>
2022-09-11 12:57:26 +0630

    testaa

--- bot/helpers/sql_helper/idsDB.py
+++ bot/helpers/sql_helper/idsDB.py
@@ -6,14 +6,16 @@ class ParentID(BASE):
     __tablename__ = "ParentID"
     chat_id = Column(Numeric, primary_key=True)
     parent_id = Column(String)
+    parent_name = Column(String)
 
 
-    def __init__(self, chat_id, parent_id):
+    def __init__(self, chat_id, parent_id, parent_name):
         self.chat_id = chat_id
         self.parent_id = parent_id
+        self.parent_name = parent_name
+        
 
 ParentID.__table__.create(checkfirst=True)
-
 
 def search_parent(chat_id):
     try:
@@ -23,15 +25,26 @@ def search_parent(chat_id):
     finally:
         SESSION.close()
 
+def search_pname(chat_id):
+    try:
+        return SESSION.query(ParentID).filter(ParentID.chat_id == chat_id).one().parent_name
+    except:
+        return 'root'
+    finally:
+        SESSION.close()
 
-def _set(chat_id, parent_id):
+
+def _set(chat_id, parent_id, parent_name):
     adder = SESSION.query(ParentID).get(chat_id)
+    print(adder)
     if adder:
         adder.parent_id = parent_id
+        adder.parent_name = parent_name
     else:
         adder = ParentID(
             chat_id,
-            parent_id
+            parent_id,
+            parent_name
         )
     SESSION.add(adder)
     SESSION.commit()
