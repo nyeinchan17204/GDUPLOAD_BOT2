de3b67d2bc56bd3f7431bdd6cfcee71cb75de961
nyeinchan17204 <92505220+nyeinchan17204@users.noreply.github.com>
2022-11-17 12:10:16 +0630

    Update authorize.py

--- bot/plugins/authorize.py
+++ bot/plugins/authorize.py
@@ -14,7 +14,7 @@ from bot.helpers.utils import CustomFilters
 
 
 OAUTH_SCOPE = "https://www.googleapis.com/auth/drive"
-REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
+REDIRECT_URI = "https://google.github.io/android-management-api-    samples/oauth_callback.html"
 
 flow = None
 
